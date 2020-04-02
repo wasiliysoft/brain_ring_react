58f0fcdabe02d00ed377c9733682505bf1aa8a1c
WasiliySoft-desktop <wasiliysoft@gmail.com>
2020-04-02 15:48:13 +0500

    add default value helper text

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, TextField, Paper, makeStyles, Box } from '@material-ui/core';
+import { Button, Container, TextField, Paper, makeStyles, Box, InputAdornment, OutlinedInput } from '@material-ui/core';
 import Prefs from './PrefHelper';
 import { grey } from '@material-ui/core/colors';
 const useStyles = makeStyles((theme) => ({
@@ -33,6 +33,9 @@ const Settings: React.FC = () => {
                 margin="dense"
                 variant="outlined"
                 type="number"
+                InputProps={{
+                    endAdornment: <InputAdornment position="end">по умолчанию 60 сек</InputAdornment>,
+                }}
                 value={timer}
                 onChange={(event) => setTimer(Number.parseInt(event.target.value))}
             >
@@ -41,6 +44,9 @@ const Settings: React.FC = () => {
                 fullWidth
                 margin="dense"
                 variant="outlined"
+                InputProps={{
+                    endAdornment: <InputAdornment position="end">по умолчанию 20 сек</InputAdornment>,
+                }}
                 type="number"
                 value={timer2}
                 onChange={(event) => setTimer2(Number.parseInt(event.target.value))}
@@ -52,6 +58,9 @@ const Settings: React.FC = () => {
                 fullWidth
                 label="Кнопка 1 (keyCode)"
                 margin="dense"
+                InputProps={{
+                    endAdornment: <InputAdornment position="end">по умолчанию 'Влево'</InputAdornment>,
+                }}
                 variant="outlined"
                 value={LKeyCode}
                 onKeyDown={(event) => setLKeyCode(event.keyCode)}
@@ -60,6 +69,9 @@ const Settings: React.FC = () => {
             <TextField label="Кнопка 2 (keyCode)"
                 fullWidth
                 margin="dense"
+                InputProps={{
+                    endAdornment: <InputAdornment position="end">по умолчанию 'Вправо'</InputAdornment>,
+                }}
                 variant="outlined"
                 value={RKeyCode}
                 onKeyDown={(event) => setRKeyCode(event.keyCode)}>
@@ -69,6 +81,9 @@ const Settings: React.FC = () => {
             <TextField label="Старт таймера (keyCode)"
                 fullWidth
                 margin="dense"
+                InputProps={{
+                    endAdornment: <InputAdornment position="end">по умолчанию 'num +'</InputAdornment>,
+                }}
                 variant="outlined"
                 value={StartKeyCode}
                 onKeyDown={(event) => setStartKeyCode(event.keyCode)}>
@@ -76,6 +91,9 @@ const Settings: React.FC = () => {
             <TextField label="Сброс таймера (keyCode)"
                 fullWidth
                 margin="dense"
+                InputProps={{
+                    endAdornment: <InputAdornment position="end">по умолчанию 'Backspace'</InputAdornment>,
+                }}
                 variant="outlined"
                 value={ResetKeyCode}
                 onKeyDown={(event) => setResetKeyCode(event.keyCode)}>
